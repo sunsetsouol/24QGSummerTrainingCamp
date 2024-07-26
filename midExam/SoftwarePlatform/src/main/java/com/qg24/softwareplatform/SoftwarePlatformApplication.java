86b76f599ff3e0debb7053fd759e2d7a650eafdb
Song He <1311471185@qq.com>
2024-07-26 15:49:49 +0800

    init

---
+++ midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/SoftwarePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.qg24.softwareplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwarePlatformApplication.class, args);
+    }
+
+}
---
+++ midExam/SoftwarePlatform/src/test/java/com/qg24/softwareplatform/SoftwarePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qg24.softwareplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwarePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
