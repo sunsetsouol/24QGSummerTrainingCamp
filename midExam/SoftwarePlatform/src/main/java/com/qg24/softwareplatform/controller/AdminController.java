638535a6dc84dc9113b47053b5d27892bda2bb2c
Xb2555 <2012753288@qq.com>
2024-07-27 22:33:34 +0800

    feature: 修改AdminController和UserController

--- midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/controller/AdminController.java
+++ midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/controller/AdminController.java
@@ -1,36 +1,38 @@
 package com.qg24.softwareplatform.controller;
 
 import com.qg24.softwareplatform.po.dto.UpdateSoftwareLatestInfoDTO;
+import com.qg24.softwareplatform.po.entity.Software;
 import com.qg24.softwareplatform.po.result.Result;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/admin")
 public class AdminController {
     /**
      * 管理员更改软件信息
      */
-    @PostMapping("/admin/updateSoftwareBasicInfo")
-    public Result<?> UpdateSoftWareBasicInfo(){
+    @PostMapping("/updateSoftwareBasicInfo")
+    public Result<?> UpdateSoftWareBasicInfo(@RequestBody Software software){
     }
 
     /**
      * 管理员修改软件最新版本信息
      */
-    @PostMapping("/admin/updateSoftwareLatestInfo")
+    @PostMapping("/updateSoftwareLatestInfo")
     public Result<?> updateSoftwareLatestInfo(@RequestBody UpdateSoftwareLatestInfoDTO updateSoftwareLatestInfoDTO){
     }
 
     /**
      * 管理员 分页 回显审核记录
      */
-    @GetMapping("/admin/showVerificationHistory")
+    @GetMapping("/showVerificationHistory")
     public Result<?> showVerificationHistory(@RequestParam("page") int page,@RequestParam("pageSize")int pageSize){
     }
 
     /**
      * 新软件/新版本  审核通过/驳回
      */
-    @PostMapping("/admin/verifyApplication")
+    @PostMapping("/verifyApplication")
     public Result<?> verifyApplication(@RequestParam("softwareInfoTempId")int softwareInfoTempId,@RequestParam("status")int status){
     }
 }
--- midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/controller/UserController.java
+++ midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/controller/UserController.java
@@ -5,10 +5,12 @@ import com.qg24.softwareplatform.po.vo.SimpleSoftwareVO;
 import com.qg24.softwareplatform.po.vo.UserApplicationRecordVO;
 import com.qg24.softwareplatform.po.vo.UserBuySoftwareVO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     /**
      * 个人页面展示个人开发的软件
@@ -20,14 +22,14 @@ public class UserController {
     /**
      * 个人页面展示申请记录
      */
-    @GetMapping("/user/applicationRecord")
+    @GetMapping("/applicationRecord")
     public Result<?> applicationRecord(@RequestParam("userId")String userId){
     }
 
     /**
      * 个人页面查询可购买过（授权过但是可能过期了）的软件(通过过期时间判断是否过期)
      */
-    @GetMapping("/user/PagedQueryAvailableSoftware")
+    @GetMapping("/PagedQueryAvailableSoftware")
     public Result<?> PagedQueryAvailableSoftware(@RequestParam("userId")String userId){
     }
 }
