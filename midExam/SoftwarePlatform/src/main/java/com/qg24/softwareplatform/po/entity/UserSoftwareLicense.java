5909211d186bfce627037b21c0fec7e8bf8a1a32
Suyyyus <2893796696@qq.com>
2024-07-28 11:07:53 +0800

    feat: 新增用户查询指纹信息功能

--- midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/po/entity/UserSoftwareLicense.java
+++ midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/po/entity/UserSoftwareLicense.java
@@ -4,11 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.OffsetDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class userSoftwareLicense {
+public class UserSoftwareLicense {
 
     private String authTime;
 
