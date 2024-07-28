15cb148e04fcdae0d7f346cc5e54441703959553
Xb2555 <2012753288@qq.com>
2024-07-28 17:04:38 +0800

    fix: 修改sql语句错误 fix: 增加softWare类的userId成员变量

--- midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/mapper/UserMapper.java
+++ midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/mapper/UserMapper.java
@@ -12,16 +12,16 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    @Select("select count(*) from software where userId=#{userId}")
+    @Select("select count(*) from software where user_id=#{userId}")
     int getTotalNumber(String userId);//获取总软件数
 
     List<SimpleSoftwareVO> getUserSoftware(String userId,int limit,int offset);//分页查询用户开发的软件
 
     List<UserApplicationRecordVO> getApplicationRecord(String userId);//获取用户申请记录
 
-    @Select("select * from user_software_auth where userId=#{userId}")
+    @Select("select * from user_software_auth where user_id=#{userId}")
     List<UserSoftwareAuth> getAvailableSoftware(String userId);//查询用户授权过的软件
 
-    @Select("select softwareName from software where softwareId=#{softwareId}")
+    @Select("select software_name from software where software_id=#{softwareId}")
     String getSoftwareName(int softwareId);//获得软件名
 }
