61c7731be8270feb4784781c30b1c0427a410f0b
Oramk <1710025170@qq.com>
2024-07-31 17:00:49 +0800

    fix: 对加密数据进行修正

--- midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/util/RSAUtil.java
+++ midExam/SoftwarePlatform/src/main/java/com/qg24/softwareplatform/util/RSAUtil.java
@@ -8,113 +8,41 @@ import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 public class RSAUtil {
-    /**
-     * 生成秘钥对
-     * @return
-     * @throws Exception
-     */
-    public static KeyPair getKeyPair() throws Exception {
+    // 生成RSA密钥对
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(2048);
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-        return keyPair;
+        return keyPairGenerator.generateKeyPair();
     }
 
-    /**
-     * 获取公钥(Base64编码)
-     * @param keyPair
-     * @return
-     */
-    public static String getPublicKey(KeyPair keyPair){
-        PublicKey publicKey = keyPair.getPublic();
-        byte[] bytes = publicKey.getEncoded();
-        return byte2Base64(bytes);
-    }
-
-    /**
-     * 获取私钥(Base64编码)
-     * @param keyPair
-     * @return
-     */
-    public static String getPrivateKey(KeyPair keyPair){
-        PrivateKey privateKey = keyPair.getPrivate();
-        byte[] bytes = privateKey.getEncoded();
-        return byte2Base64(bytes);
-    }
-
-    /**
-     * 将Base64编码后的公钥转换成PublicKey对象
-     * @param pubStr
-     * @return
-     * @throws Exception
-     */
-    public static PublicKey string2PublicKey(String pubStr) throws Exception{
-        byte[] keyBytes = base642Byte(pubStr);
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PublicKey publicKey = keyFactory.generatePublic(keySpec);
-        return publicKey;
-    }
-
-    /**
-     * 将Base64编码后的私钥转换成PrivateKey对象
-     * @param priStr
-     * @return
-     * @throws Exception
-     */
-    public static PrivateKey string2PrivateKey(String priStr) throws Exception{
-        byte[] keyBytes = base642Byte(priStr);
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-        return privateKey;
-    }
-
-    /**
-     * 公钥加密
-     * @param content
-     * @param publicKey
-     * @return
-     * @throws Exception
-     */
-    public static byte[] publicEncrypt(byte[] content, PublicKey publicKey) throws Exception{
+    // 使用公钥加密数据
+    public static byte[] encrypt(byte[] data, PublicKey publicKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] bytes = cipher.doFinal(content);
-        return bytes;
+        return cipher.doFinal(data);
     }
 
-    /**
-     * 私钥解密
-     * @param content
-     * @param privateKey
-     * @return
-     * @throws Exception
-     */
-    public static byte[] privateDecrypt(byte[] content, PrivateKey privateKey) throws Exception{
+    // 使用私钥解密数据
+    public static byte[] decrypt(byte[] data, PrivateKey privateKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] bytes = cipher.doFinal(content);
-        return bytes;
+        return cipher.doFinal(data);
     }
 
-    /**
-     * 字节数组转Base64编码
-     * @param bytes
-     * @return
-     */
-    public static String byte2Base64(byte[] bytes){
-        return Base64.getEncoder().encodeToString(bytes);
+    // 将Base64编码的公钥字符串转换为PublicKey对象
+    public static PublicKey string2PublicKey(String publicKeyStr) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(publicKeyStr);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(keySpec);
     }
 
-    /**
-     * Base64编码转字节数组
-     * @param base64Key
-     * @return
-     * @throws IOException
-     */
-    public static byte[] base642Byte(String base64Key) {
-        return Base64.getDecoder().decode(base64Key);
+    // 将Base64编码的私钥字符串转换为PrivateKey对象
+    public static PrivateKey string2PrivateKey(String privateKeyStr) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(privateKeyStr);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(keySpec);
     }
 
 }
