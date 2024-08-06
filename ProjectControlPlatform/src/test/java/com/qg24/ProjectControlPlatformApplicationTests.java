4bffc7348681b9fbb4f474d200c3e9222ddfd557
Oramk <1710025170@qq.com>
2024-08-06 15:46:11 +0800

    feat: 新建项目

---
+++ ProjectControlPlatform/src/test/java/com/qg24/ProjectControlPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qg24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectControlPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
